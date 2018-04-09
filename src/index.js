a9a6bb9bf71234ed4fb71822e15686d2547ca732
Alec Bernardi <bernaa2@rpi.edu>
2018-04-09 16:00:31 -0400

    Fix formatting

--- src/index.js
+++ src/index.js
@@ -3,124 +3,119 @@
 // enter command: chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
 // the window that opens allow the security to be bypassed
 
-var request = require('request'); // "Request" library
+const request = require('request'); // "Request" library
 
-var client_id = 'ea788a5aa0034d16a33d9d60488f2f45'; // Your client id
-var client_secret = '2a21c40782a34a71bb28701440b287be'; // Your secret
+const clientId = 'ea788a5aa0034d16a33d9d60488f2f45'; // Your client id
+const clientSecret = '2a21c40782a34a71bb28701440b287be'; // Your secret
 
 // Application requests authorization
-var authOptions = {
+const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    Authorization: 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
   },
   form: {
-    grant_type: 'client_credentials'
+    grant_type: 'client_credentials' // eslint-disable-line camelcase
   },
   json: true
 };
 
-// the 
-var id = "0";
-var info = null;
-var features = null;
+let id = '0';
+let info = null;
+let features = null;
 
-var trackNameToID = function(trackName, artist, callback) {
-	var trackNameFormatted = trackName.split(' ').join('+')
-	var artistNameFormatted = trackName.split(' ').join('+')
-	var query = trackNameFormatted + '+' + artistNameFormatted
-	var requestUrl = 'https://api.spotify.com/v1/search/?q=' + query + '&type=track';
-	request.post(authOptions, function(error, response, body) {
-	  if (!error && response.statusCode === 200) {
-
-	    // use the access token to access the Spotify Web API
-	    var token = body.access_token;
-	    var options = {
-	      url: requestUrl,
-	      headers: {
-	        'Authorization': 'Bearer ' + token
-	      },
-	      json: true
-	    };
-	    request.get(options, function(error, response, body) {
-	      	id = body.tracks.items[0].id;
-	      	callback(id);
-	    });
-	  }
-	});
+function trackNameToID(trackName, artist, callback) {
+  const trackNameFormatted = trackName.split(' ').join('+');
+  const artistNameFormatted = trackName.split(' ').join('+');
+  const query = trackNameFormatted + '+' + artistNameFormatted;
+  const requestUrl = 'https://api.spotify.com/v1/search/?q=' + query + '&type=track';
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      // Use the access token to access the Spotify Web API
+      const token = body.access_token;
+      const options = {
+        url: requestUrl,
+        headers: {
+          Authorization: 'Bearer ' + token
+        },
+        json: true
+      };
+      request.get(options, (error, response, body) => {
+        id = body.tracks.items[0].id;
+        callback(id);
+      });
+    }
+  });
 }
 
-var trackNameToTrackInfo = function(trackName, artist, callback) {
-	trackNameToID(trackName, artist, function(trackID) {
-		var requestUrl = 'https://api.spotify.com/v1/tracks/' + trackID;
-		request.post(authOptions, function(error, response, body) {
-		  if (!error && response.statusCode === 200) {
-
-		    // use the access token to access the Spotify Web API
-		    var token = body.access_token;
-		    var options = {
-		      url: requestUrl,
-		      headers: {
-		        'Authorization': 'Bearer ' + token
-		      },
-		      json: true
-		    };
-		    request.get(options, function(error, response, body) {
-		      info = body;
-		      callback(info);
-		    });
-		  }
-		});
-	})
+function trackNameToTrackInfo(trackName, artist, callback) {
+  trackNameToID(trackName, artist, trackID => {
+    const requestUrl = 'https://api.spotify.com/v1/tracks/' + trackID;
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        // Use the access token to access the Spotify Web API
+        const token = body.access_token;
+        const options = {
+          url: requestUrl,
+          headers: {
+            Authorization: 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, (error, response, body) => {
+          info = body;
+          callback(info);
+        });
+      }
+    });
+  });
 }
 
-var trackNameToFeatures = function(trackName, artist, callback) {
-	trackNameToID(trackName, artist, function(trackID) {
-		request.post(authOptions, function(error, response, body) {
-		  if (!error && response.statusCode === 200) {
-
-		    // use the access token to access the Spotify Web API
-		    var token = body.access_token;
-		    var options = {
-		      url: 'https://api.spotify.com/v1/audio-features/' + trackID,
-		      headers: {
-		        'Authorization': 'Bearer ' + token
-		      },
-		      json: true
-		    };
-		    request.get(options, function(error, response, body) {
-		      	features = body;
-		      	callback(features);
-		    });
-		  }
-		});
-	}) 
+function trackNameToFeatures(trackName, artist, callback) {
+  trackNameToID(trackName, artist, trackID => {
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        // Use the access token to access the Spotify Web API
+        const token = body.access_token;
+        const options = {
+          url: 'https://api.spotify.com/v1/audio-features/' + trackID,
+          headers: {
+            Authorization: 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, (error, response, body) => {
+          features = body;
+          callback(features);
+        });
+      }
+    });
+  });
 }
 
-var allTracks = [];
-var allFeatures = [];
+const allTracks = [];
+const allFeatures = [];
 
-trackNameToTrackInfo("Castle of Glass", "Linkin Park", function(info) {
-	allTracks.push(info);
+trackNameToTrackInfo('Castle of Glass', 'Linkin Park', info => {
+  allTracks.push(info);
 });
-trackNameToFeatures("Castle of Glass", "Linkin Park", function(features) {
-	allFeatures.push(features);
-});
-
-trackNameToTrackInfo("In the End", "Linkin Park", function(info) {
-	allTracks.push(info);
-});
-trackNameToFeatures("In the End", "Linkin Park", function(features) {
-	allFeatures.push(features);
+trackNameToFeatures('Castle of Glass', 'Linkin Park', features => {
+  allFeatures.push(features);
 });
 
-trackNameToTrackInfo("Livin on a Prayer", "Bon Jovi", function(info) {
-	allTracks.push(info);
+trackNameToTrackInfo('In the End', 'Linkin Park', info => {
+  allTracks.push(info);
 });
-trackNameToFeatures("Livin on a Prayer", "Bon Jovi", function(features) {
-	allFeatures.push(features);
+trackNameToFeatures('In the End', 'Linkin Park', features => {
+  allFeatures.push(features);
 });
 
-console.log(allTracks)
-console.log(allFeatures)
+trackNameToTrackInfo('Livin on a Prayer', 'Bon Jovi', info => {
+  allTracks.push(info);
+});
+trackNameToFeatures('Livin on a Prayer', 'Bon Jovi', features => {
+  allFeatures.push(features);
+});
 
+console.log(allTracks);
+console.log(allFeatures);
