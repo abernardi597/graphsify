0b29175299e5be7c25bb7508f7841f8425dcf23c
Alec Bernardi <bernaa2@rpi.edu>
2018-04-11 10:40:54 -0400

    Add Promise-based track fetching with axios

--- src/index.js
+++ src/index.js
@@ -5,29 +5,5 @@
 
 import spotify from './data/spotify';
 
-const allTracks = [];
-const allFeatures = [];
-
-spotify.trackNameToTrackInfo('Castle of Glass', 'Linkin Park', info => {
-  allTracks.push(info);
-});
-spotify.trackNameToFeatures('Castle of Glass', 'Linkin Park', features => {
-  allFeatures.push(features);
-});
-
-spotify.trackNameToTrackInfo('In the End', 'Linkin Park', info => {
-  allTracks.push(info);
-});
-spotify.trackNameToFeatures('In the End', 'Linkin Park', features => {
-  allFeatures.push(features);
-});
-
-spotify.trackNameToTrackInfo('Livin on a Prayer', 'Bon Jovi', info => {
-  allTracks.push(info);
-});
-spotify.trackNameToFeatures('Livin on a Prayer', 'Bon Jovi', features => {
-  allFeatures.push(features);
-});
-
-console.log(allTracks);
-console.log(allFeatures);
+spotify.fetchTrack('3twNvmDtFQtAd5gMKedhLD').then(console.log);
+spotify.searchTracks('Love').then(console.log);
