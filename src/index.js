5ef40b6aaaa1dde723d61a99ca977f44ad34116a
Alec Bernardi <bernaa2@rpi.edu>
2018-04-12 18:48:30 -0400

    Add Visualization with faux DOM sandbox for D3

--- src/index.js
+++ src/index.js
@@ -2,23 +2,13 @@
 // open windows "run"
 // enter command: chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
 // the window that opens allow the security to be bypassed
+import React from 'react';
+import {render} from 'react-dom';
+
+import Visualization from './ui/vis';
 
 import spotify from './data/spotify';
-import * as Features from './data/features';
 
-spotify.fetchTracks(['3twNvmDtFQtAd5gMKedhLD']).then(console.log);
-spotify.searchTracks('Love').then(console.log);
-
-const weights = {
-  energy: 0.9,
-  valence: 0.8,
-  danceability: 0.7,
-  tempo: 0.6
-};
-
-spotify.fetchTracks(['6rVJfgSjj2jEKkXL5QuXPd']).then(tracks => {
-  console.log(tracks[0]);
-  const req = spotify.recommendTracks(tracks[0], weights, 10);
-  req.then(console.log);
-  req.then(suggested => suggested.map(track => Features.distance(tracks[0].features, track.features, weights))).then(console.log);
-});
+Promise.all([spotify.searchTracks('Love'), spotify.searchTracks('Hate')]).then(res =>
+  render(<Visualization active={res[0]} suggested={res[1]}/>, document.querySelector('#root'))
+);
---
+++ src/ui/vis.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactFauxDOM, {withFauxDOM} from 'react-faux-dom';
+
+class Visualization extends React.Component {
+  componentDidMount() {
+    const faux = this.props.connectFauxDOM('svg', 'rd3');
+    const {active, suggested} = this.props;
+    console.log(active);
+    console.log(suggested);
+    const txt = new ReactFauxDOM.Element('text', faux);
+    txt.setAttribute('x', 0);
+    txt.setAttribute('y', 15);
+    txt.innerHTML = 'D3 goes here';
+    faux.appendChild(txt);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.rd3}
+      </div>
+    );
+  }
+}
+
+Visualization.propTypes = {
+  rd3: PropTypes.object,
+  connectFauxDOM: PropTypes.func,
+  active: PropTypes.array.isRequired,
+  suggested: PropTypes.array.isRequired
+};
+
+export default withFauxDOM(Visualization);
