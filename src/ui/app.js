07d9b7074243f510fd43d9535b9b6141001ed845
Alec Bernardi <bernaa2@rpi.edu>
2018-04-23 12:45:52 -0400

    Add Settings Drawer

--- src/ui/app.js
+++ src/ui/app.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import {createMuiTheme, MuiThemeProvider, withStyles} from 'material-ui/styles';
 
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconSettings from 'material-ui-icons/Settings';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
@@ -31,11 +33,13 @@ const style = theme => ({
     overflow: 'hidden',
     display: 'flex'
   },
+  flex: {
+    flex: 1
+  },
   bar: {
     zIndex: theme.zIndex.drawer + 1
   },
   drawer: {
-    position: 'relative',
     width: 400
   },
   toolbar: theme.mixins.toolbar
@@ -47,13 +51,14 @@ class App extends React.Component {
     this.state = {
       active: [],
       suggested: [],
-      adding: false
+      adding: false,
+      sliding: false
     };
   }
 
   render() {
     const {classes} = this.props;
-    const {active, suggested, adding} = this.state;
+    const {active, suggested, adding, sliding} = this.state;
     const add = track => {
       if (!active.includes(track)) {
         active.push(track);
@@ -66,19 +71,28 @@ class App extends React.Component {
     const close = () => {
       this.setState({adding: false});
     };
+    const slide = () => {
+      this.setState({sliding: !sliding});
+    };
     return (
       <MuiThemeProvider theme={theme}>
         <div className={classes.root}>
           <AppBar position="absolute" className={classes.bar}>
             <Toolbar>
-              <Typography variant="title" color="inherit" noWrap>
+              <Typography variant="title" color="inherit" noWrap className={classes.flex}>
                 Graphsify
               </Typography>
+              <IconButton onClick={slide}>
+                <IconSettings color={sliding ? 'secondary' : 'inherit'}/>
+              </IconButton>
             </Toolbar>
           </AppBar>
           <Drawer variant="permanent" classes={{paper: classes.drawer}}>
             <div className={classes.toolbar} />
             <Library active={active} suggested={suggested} fabAdd={open}/>
+          </Drawer>
+          <Drawer variant="persistent" anchor="right" classes={{paper: classes.drawer}} open={sliding}>
+            <div className={classes.toolbar} />
           </Drawer>
           <main className={classes.content}>
             <div className={classes.toolbar} />
