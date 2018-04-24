59e6b7e46340b270e75a044348e82ec1c97cf44a
Alec Bernardi <bernaa2@rpi.edu>
2018-04-23 23:08:14 -0400

    Remove normalize

--- src/data/features/feature.js
+++ src/data/features/feature.js
@@ -5,10 +5,6 @@ export default class Feature {
     this.extract = extractor;
   }
 
-  normalize(value) {
-    return value;
-  }
-
   distance(v1, v2) {
     const diff = (v2 - v1) / (this.max - this.min);
     return diff * diff;
